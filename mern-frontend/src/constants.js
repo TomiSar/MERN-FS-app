0be07e88bdb31f7d19b2eb8de91c90f25b6f7d2c
Tomi Sarjamo <Tomi.Sarjamo@softability.fi>
2024-03-21 10:26:41 +0200

    Missing files included in this commit.

---
+++ mern-frontend/src/constants.js
@@ -0,0 +1,4 @@
+// API endpoints Users
+export const API_USER_LOGIN_URL = 'http://localhost:5000/api/users/login';
+export const API_USER_SIGN_UP_URL = 'http://localhost:5000/api/users/signup';
+export const API_GET_USERS_URL = 'http://localhost:5000/api/users/';
---
+++ mern-frontend/src/shared/hooks/http-hook.js
@@ -0,0 +1,58 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = 'GET', headers = {}, body = null) => {
+      setIsLoading(true);
+      const httpAbortControl = new AbortController();
+      activeHttpRequests.current.push(httpAbortControl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          headers,
+          body,
+          signal: httpAbortControl.signal,
+        });
+
+        const responseData = await response.json();
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqControl) => reqControl !== httpAbortControl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        if (err.name === 'AbortError') {
+          return;
+        }
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    const clean = () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+    return clean;
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
